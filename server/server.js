bd18a4da887ab6d13ca8f03a5724ffa87db317be
Despicable-at <agyemangtakyi2021@gmail.com>
2025-05-06 01:12:18 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
