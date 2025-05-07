7987c75b3a808b22f5ebcf9dd0b62c9e445afbf2
Despicable-at <agyemangtakyi2021@gmail.com>
2025-05-07 01:13:28 +0000

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import CampaignCard from '../components/CampaignCard';
 import Footer from '../components/Footer';
