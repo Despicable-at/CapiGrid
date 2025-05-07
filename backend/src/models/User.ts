485764b119452442e04263821f1aee5e8d927002
Despicable-at <agyemangtakyi2021@gmail.com>
2025-05-07 18:54:40 +0000

    Update User.ts

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -12,6 +12,7 @@ const userSchema = new Schema<IUser>(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
   },
   { timestamps: { createdAt: true, updatedAt: false } }
 );
